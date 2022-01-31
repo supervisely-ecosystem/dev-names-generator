9fdbecf036381dd0441514d98337829fef6dbda0
maxim kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-01-31 09:51:47 +0200

    not tested

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import sys
 from pathlib import Path
@@ -9,7 +10,7 @@ from fastapi.responses import JSONResponse
 import supervisely as sly
 
 from supervisely.fastapi_helpers import ShutdownMiddleware, shutdown, \
-                                        WebsocketMiddleware, \
+                                        WebsocketMiddleware, WebsocketManager, \
                                         StateMiddleware, \
                                         DataMiddleware
 from supervisely.fastapi_helpers import Jinja2Templates
@@ -24,71 +25,75 @@ from asgiref.sync import async_to_sync
 # print(f"App root directory: {app_dir}")
 # sys.path.append(app_dir)
 
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
+
 
 app = FastAPI()
 
-from starlette.middleware.base import BaseHTTPMiddleware
+
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+# from fastapi.middleware.trustedhost import TrustedHostMiddleware
+# from fastapi.middleware.gzip import GZipMiddleware
+# from starlette.middleware.base import BaseHTTPMiddleware
 # app.add_middleware(BaseHTTPMiddleware, dispatch=func)
 
-# app.add_middleware(WebsocketMiddleware) # sub app #@TODO - reimplement?????
+app.add_middleware(WebsocketMiddleware)
 app.add_middleware(ShutdownMiddleware) 
-app.add_middleware(StateMiddleware)     # sub app #@TODO - reimplement
-# app.add_middleware(DataMiddleware)      # sub app #@TODO - reimplement to route???
+app.add_middleware(StateMiddleware)
+app.add_middleware(DataMiddleware)
 # remove classmethods??? and reimplement to instance methods?
 
 templates = Jinja2Templates(directory="templates")
 
-#@TODO: middleware broken request 
-# can not use multiple times
-# request = Request(scope, receive, send)
-#@TODO: statejson as depends
 
 @app.get("/")
 async def read_index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
-# @app.middleware("http")
-# async def add_process_time_header(request: Request, call_next):
-#     start_time = time.time()
-#     response = await call_next(request)
-#     process_time = time.time() - start_time
-#     response.headers["X-Process-Time"] = str(process_time)
-#     return response
 
-# @app.post("/sync-generate")
-# def sync_generate(request: Request):
-#     # example how to execute ASYNC methods in SYNC function
-#     state = async_to_sync(request.json)()
-#     time.sleep(5)
-#     state["name"] = names.get_first_name()
-#     async_to_sync(state.synchronize_changes())
+@app.post("/sync-generate")
+def sync_generate(request: Request):
+    # example how to execute ASYNC methods in SYNC function
+    json_body = async_to_sync(request.json)()
+    time.sleep(1.5)
+    # state["name"] = names.get_first_name()
+    # async_to_sync(state.synchronize_changes())
 
 
 @app.post("/generate")
-async def generate(request: Request):
-    state = await request.json()
-    #state = await StateJson.from_request(request)
-    x = 10
-    x += 1
-    # state["name"] = names.get_first_name()
-    # state.synchronize_changes()
+async def generate(request: Request, state: StateJson = Depends(StateJson.from_request)):
+    state["name"] = names.get_first_name()
+    return state.get_changes()
 
 
 @app.post("/generate-ws")
-async def generate_ws(request: Request):
-    pass
-    # state = await request.json()
-    # await ws_manager.broadcast({'name': names.get_first_name()})
+async def generate_ws(request: Request, state: StateJson = Depends(StateJson.from_request)):
+    state["name"] = names.get_first_name()
+    await state.synchronize_changes()
+    # or
+    # await WebsocketManager().broadcast(state.get_changes())
+
+
+@app.post("/do-then-shutdown")
+async def do_then_shutdown(request: Request, state: StateJson = Depends(StateJson.from_request)):
+    print("do something here and then manual shutdown")
+    shutdown(app)
+    # or 
+    # shutdown(request.app)
+
+
+@app.post("/count")
+async def count(request: Request, state: StateJson = Depends(StateJson.from_request)):
+    for i in range(10):
+        asyncio.sleep(1)
+        state["counter"] = i
+        state.synchronize_changes()
 
 
 @app.on_event("startup")
 def startup_event():
-    print("init something before server starts")
+    print("startup_event --- init something before server starts")
 
 
 @app.on_event("shutdown")
 def shutdown_event():
-    print("do something before server shutdowns")
+    print("shutdown_event --- do something before server shutdowns")
